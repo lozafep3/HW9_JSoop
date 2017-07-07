5686144b40e2a171b43f4ae0266aa432af275b25
Arthur Loza <loza.uns@gmail.com>
2017-07-07 23:01:10 +0300

    HW9_JSoop: clock 1.0

--- scripts.js
+++ scripts.js
@@ -1,37 +1,47 @@
+function clock() {
+    function Clock(timezone, id, format) {
+        document.getElementById(id).onclick = function () {
+            format = !format;
+        };
 
-function Clock(timezone, id) {
-    //this.format = 'hh:mm:ss';
-    this.draw = function() {
-        document.getElementById(id).innerHTML = this.getTime(timezone);
+        this.draw = function () {
+            document.getElementById(id).innerHTML = this.getTime(timezone, format);
+        };
     }
+
+    //Set new timezones here:
+    var timeUTC = new Clock(-2, 'time_utc', true);
+    var timeCET = new Clock(-1, 'time_cet', true);
+    var timeUTC2 = new Clock(0, 'time_utc2', true);
+
+    Clock.prototype.getTime = function getTime(timezone, format) {
+        var cDate = new Date(),
+            cHours = cDate.getHours() + timezone,
+            cMinutes = cDate.getMinutes(),
+            cSeconds = cDate.getSeconds();
+
+        if (cDate.getHours() < 10) {
+            cHours = '0' + cDate.getHours() + timezone;
+        }
+        if (cDate.getMinutes() < 10) {
+            cMinutes = '0' + cDate.getMinutes();
+        }
+        if (cDate.getSeconds() < 10) {
+            cSeconds = '0' + cDate.getSeconds();
+        }
+
+        if (format === true) {
+            return cHours + ':' + cMinutes + ':' + cSeconds;
+        } else {
+            return cHours + ':' + cMinutes;
+        }
+    };
+
+    setInterval(function () {
+        timeUTC.draw();
+        timeCET.draw();
+        timeUTC2.draw();
+    }, 1000);
 }
 
-//Set new timezones here:
-var timeUTC = new Clock(-2, 'time_utc');
-var timeCET = new Clock(-1, 'time_cet');
-var timeUTC2 = new Clock(0, 'time_utc2');
-
-Clock.prototype.getTime = function getTime(timezone) {
-    var cDate = new Date(),
-        cHours = cDate.getHours() + timezone,
-        cMinutes = cDate.getMinutes(),
-        cSeconds = cDate.getSeconds();
-
-    if (cDate.getHours() < 10) {
-        cHours = '0' + cDate.getHours() + timezone;
-    }
-    if (cDate.getMinutes() < 10) {
-        cMinutes = '0' + cDate.getMinutes();
-    }
-    if (cDate.getSeconds() < 10) {
-        cSeconds = '0' + cDate.getSeconds();
-    }
-
-    return cHours + ':' + cMinutes + ':' + cSeconds;
-};
-
-setInterval(function() {
-    timeUTC.draw();
-    timeCET.draw();
-    timeUTC2.draw();
-}, 1000);
+clock();
